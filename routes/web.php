9c6c33c87e6b082dbc2dceaee2ee633ba957bb5a
Mauricio Rey <mauricioreygmail.com>
2024-03-16 20:43:45 -0500

    routes y models

--- routes/web.php
+++ routes/web.php
@@ -3,29 +3,41 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/','welcome')->name('welcome');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+//creaciosn de  ruta chirps
+Route::get('/chirps', function () {
+    return 'welcome a la pagina  de chirps';
+})->name('chirps.index');
+
+//Ruta opoer defefcto de breeze
+
+//verified  es para verificar los correos  app/models/user 
 
 Route::middleware('auth')->group(function () {
+    Route::view ('/dashboard', 'dashboard')->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/chirps', function () {
+        return 'welcome a la pagina  de chirps';
+    })->name('chirps.index');
 });
 
 require __DIR__.'/auth.php';
+
+//se pueden pasar parametros en las rutas
+/*Route::get('/chirps/{chirp?}', function ($chirp=null) {
+    return 'welcome a la pagina  de chirps' . $chirp;
+});*/
+
+//se pueden hacer redirecciones parametros en las rutas
+/*Route::get('/chirps/{chirp}', function ($chirp) {
+    if ($chirp=== '2') {
+        return to_route('chirps.index');        
+    }
+    return 'Chirps Detail' . $chirp;
+});*/
